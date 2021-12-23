7033ef55052725fad26fe620dd86ec80738fc69c
Kamil Domański <kamil@domanski.co>
2021-12-23 17:07:17 +0100

    actually, separating the two checks made no sense ;-)

--- zsh-brew-autoupdate.plugin.zsh
+++ zsh-brew-autoupdate.plugin.zsh
@@ -4,7 +4,7 @@ function current_epoch() {
 }
 
 function update_last_updated_file() {
-    local LAST_UPDATE_EPOCH LAST_OUTDATED_EPOCH
+    local LAST_UPDATE_EPOCH
 
     if [[ -f "${ZSH_CACHE_DIR}/.brew-auto-update" ]]; then
         source "${ZSH_CACHE_DIR}/.brew-auto-update"
@@ -14,11 +14,7 @@ function update_last_updated_file() {
         LAST_UPDATE_EPOCH="$1"
     fi
 
-    if [[ -n "$2" ]]; then
-        LAST_OUTDATED_EPOCH="$2"
-    fi
-
-    echo "LAST_UPDATE_EPOCH=${LAST_UPDATE_EPOCH}\nLAST_OUTDATED_EPOCH=${LAST_OUTDATED_EPOCH}" >! "${ZSH_CACHE_DIR}/.brew-auto-update"
+    echo "LAST_UPDATE_EPOCH=${LAST_UPDATE_EPOCH}" >! "${ZSH_CACHE_DIR}/.brew-auto-update"
 }
 
 function update_homebrew() {
@@ -30,41 +26,22 @@ function update_homebrew() {
     #  the shell actually exits what it's running.
     trap "
         ret=\$?
-        unset -f current_epoch update_last_updated_file update_homebrew check_outdated
+        unset -f current_epoch update_last_updated_file update_homebrew
         command rm -rf '$ZSH/log/brew-auto-update.lock'
         return \$ret
     " EXIT INT QUIT
 
     if brew update &>/dev/null; then
-        # update only the 'brew update' epoch
-        update_last_updated_file "$(current_epoch)" ""
-    fi
-}
-
-function check_outdated() {
-    # Remove lock directory on exit. `return $ret` is important for when trapping a SIGINT:
-    #  The return status from the function is handled specially. If it is zero, the signal is
-    #  assumed to have been handled, and execution continues normally. Otherwise, the shell
-    #  will behave as interrupted except that the return status of the trap is retained.
-    #  This means that for a CTRL+C, the trap needs to return the same exit status so that
-    #  the shell actually exits what it's running.
-    trap "
-        ret=\$?
-        unset -f current_epoch update_last_updated_file update_homebrew check_outdated
-        command rm -rf '$ZSH/log/brew-auto-update.lock'
-        return \$ret
-    " EXIT INT QUIT
-
-    if brew outdated 2>/dev/null > "${ZSH_CACHE_DIR}/.brew-outdated" ; then
-        # update only the 'brew outdated' epoch
-        update_last_updated_file "" "$(current_epoch)"
+        if brew outdated 2>/dev/null > "${ZSH_CACHE_DIR}/.brew-outdated" ; then
+            update_last_updated_file "$(current_epoch)"
+        fi
     fi
 }
 
 () {
     emulate -L zsh
 
-    local epoch_target mtime LAST_UPDATE_EPOCH LAST_OUTDATED_EPOCH
+    local epoch_target mtime LAST_UPDATE_EPOCH
 
     # Remove lock directory if older than a day
     zmodload zsh/datetime
@@ -76,8 +53,8 @@ function check_outdated() {
     fi
 
     # Create or update .brew-auto-update file if missing or malformed
-    if ! source "${ZSH_CACHE_DIR}/.brew-auto-update" 2>/dev/null || [[ -z "$LAST_UPDATE_EPOCH" ]] || [[ -z "$LAST_OUTDATED_EPOCH" ]]; then
-        update_last_updated_file "$(current_epoch)" "$(current_epoch)"
+    if ! source "${ZSH_CACHE_DIR}/.brew-auto-update" 2>/dev/null || [[ -z "$LAST_UPDATE_EPOCH" ]]; then
+        update_last_updated_file "$(current_epoch)"
         return
     fi
 
@@ -91,23 +68,8 @@ function check_outdated() {
             return
         fi
 
-        echo "[zsh-brew-autoupdate] Starting a Homebrew update in background."
+        echo "[zsh-brew-autoupdate] Starting a Homebrew update check in background."
         update_homebrew &>/dev/null &!
-        return
-    fi
-
-    ### brew outdated
-    # check updates every 1 hour
-    epoch_target=1
-
-    if (( ( $(current_epoch) - $LAST_OUTDATED_EPOCH ) >= $epoch_target )); then
-        # Check for lock directory
-        if ! command mkdir "$ZSH/log/brew-auto-update.lock" 2>/dev/null; then
-            return
-        fi
-
-        echo "[zsh-brew-autoupdate] Starting a Homebrew check for outdated packages in background."
-        check_outdated &>/dev/null &!
         return
     fi
 
@@ -129,4 +91,4 @@ function check_outdated() {
     fi
 }
 
-unset -f current_epoch update_last_updated_file update_homebrew check_outdated
+unset -f current_epoch update_last_updated_file update_homebrew
